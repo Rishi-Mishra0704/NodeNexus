58c81f970d65f617f72b44805f5b5c4b497c3373
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-04-06 01:13:31 +0530

    remove all changes

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Rishi-Mishra0704/NodeNexus/p2p"
 )
 
 func main() {
-	s := p2p.NewServer()
-	s.Commands = make(chan p2p.Command)
-	fmt.Println("Starting server on :8080")
+	fmt.Println("NodeNexus")
 }
